44128b068a279f5d6a01a86f26ff17744045b835
saylos <dipane.fabio@gmail.com>
2018-03-01 11:03:42 +0100

    update

--- app/src/main/java/com/example/android/mybusinessapp/MainActivity.java
+++ app/src/main/java/com/example/android/mybusinessapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.mybusinessapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendEmail (View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:dipane.fabio@gmail.com"));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
